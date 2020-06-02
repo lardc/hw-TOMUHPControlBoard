6ced9fd6813809d057dc5516a74c379011f13636
asytyi <alexander.a.sytyi@gmail.com>
2020-06-02 17:21:51 +0300

    Переработана инициализация портов GPIO на функции-обёртки

--- Firmware/Source/Board.h
+++ Firmware/Source/Board.h
@@ -1,7 +1,6 @@
 #ifndef __BOARD_H
 #define __BOARD_H
 
-#include "Delay.h"
 #include "stm32f30x.h"
 
 #include "ZwRCC.h"
@@ -16,5 +15,46 @@
 #include "ZwIWDG.h"
 #include "ZwNFLASH.h"
 #include "ZwSPI.h"
+
+// Аналоговые входы
+GPIO_PortPinSettingMacro GPIO_ANLG_I_DUT	= {GPIOA, Pin_1};
+
+// Определения для выходных портов
+GPIO_PortPinSettingMacro GPIO_FAN			= {GPIOA, Pin_0};
+GPIO_PortPinSettingMacro GPIO_M_RESET		= {GPIOA, Pin_2};
+GPIO_PortPinSettingMacro GPIO_LOAD			= {GPIOA, Pin_3};
+GPIO_PortPinSettingMacro GPIO_CS			= {GPIOA, Pin_4};
+GPIO_PortPinSettingMacro GPIO_IND			= {GPIOA, Pin_7};
+GPIO_PortPinSettingMacro GPIO_SYNC			= {GPIOA, Pin_8};
+GPIO_PortPinSettingMacro GPIO_SFTY_EN		= {GPIOA, Pin_15};
+GPIO_PortPinSettingMacro GPIO_RLC			= {GPIOB, Pin_0};
+GPIO_PortPinSettingMacro GPIO_RELAY			= {GPIOB, Pin_1};
+GPIO_PortPinSettingMacro GPIO_SNC_TOCU		= {GPIOB, Pin_2};
+GPIO_PortPinSettingMacro GPIO_SREG_CS		= {GPIOB, Pin_5};
+GPIO_PortPinSettingMacro GPIO_CS_GD2		= {GPIOB, Pin_6};
+GPIO_PortPinSettingMacro GPIO_TRIG_RST		= {GPIOB, Pin_7};
+GPIO_PortPinSettingMacro GPIO_CS_DAC		= {GPIOB, Pin_9};
+GPIO_PortPinSettingMacro GPIO_SREG_OE		= {GPIOB, Pin_11};
+GPIO_PortPinSettingMacro GPIO_CS_GD1		= {GPIOB, Pin_12};
+GPIO_PortPinSettingMacro GPIO_LDAC			= {GPIOB, Pin_14};
+GPIO_PortPinSettingMacro GPIO_LED			= {GPIOC, Pin_13};
+GPIO_PortPinSettingMacro GPIO_SYNC_GD		= {GPIOC, Pin_14};
+GPIO_PortPinSettingMacro GPIO_PS_EN			= {GPIOC, Pin_15};
+
+// Определения для портов альтернативных функций
+GPIO_PortPinSettingMacro GPIO_ALT_CAN_RX	= {GPIOA, Pin_11};
+GPIO_PortPinSettingMacro GPIO_ALT_CAN_TX	= {GPIOA, Pin_12};
+GPIO_PortPinSettingMacro GPIO_ALT_UART_RX	= {GPIOA, Pin_10};
+GPIO_PortPinSettingMacro GPIO_ALT_UART_TX	= {GPIOA, Pin_9};
+GPIO_PortPinSettingMacro GPIO_ALT_SPI1_CLK	= {GPIOA, Pin_5};
+GPIO_PortPinSettingMacro GPIO_ALT_SPI1_MISO	= {GPIOA, Pin_6};
+GPIO_PortPinSettingMacro GPIO_ALT_SPI2_CLK	= {GPIOB, Pin_13};
+GPIO_PortPinSettingMacro GPIO_ALT_SPI2_MOSI	= {GPIOB, Pin_15};
+
+// Определения для входных портов
+GPIO_PortPinSettingMacro GPIO_SFTY			= {GPIOB, Pin_3};
+GPIO_PortPinSettingMacro GPIO_OVERFLOW90	= {GPIOB, Pin_4};
+GPIO_PortPinSettingMacro GPIO_OVERFLOW10	= {GPIOB, Pin_8};
+GPIO_PortPinSettingMacro GPIO_PRESSURE		= {GPIOB, Pin_10};
 
 #endif // __BOARD_H
--- Firmware/Source/Controller/InitConfig.c
+++ Firmware/Source/Controller/InitConfig.c
@@ -1,10 +1,8 @@
 #include <InitConfig.h>
 
-
 // Forward functions
 //
 void CONTROL_Init();
-
 
 // Functions
 //
@@ -19,7 +17,7 @@ void EI_Config()
 	// Overflow 90%
 	EXTI_Config(EXTI_PB, EXTI_4, FALL_TRIG, 0);
 	EXTI_EnableInterrupt(EXTI4_IRQn, 0, true);
-
+	
 	// Overflow 10%
 	EXTI_Config(EXTI_PB, EXTI_8, FALL_TRIG, 0);
 	EXTI_EnableInterrupt(EXTI9_5_IRQn, 0, true);
@@ -32,77 +30,49 @@ void IO_Config()
 	RCC_GPIO_Clk_EN(PORTA);
 	RCC_GPIO_Clk_EN(PORTB);
 	RCC_GPIO_Clk_EN(PORTC);
-
+	
 	// Аналоговые входы
-	GPIO_Config (GPIOA, Pin_1, Analog, NoPull, HighSpeed, NoPull);				// PA1 - I_DUT
-
+	GPIO_InitAnalog(GPIO_ANLG_I_DUT);
+	
 	// Выходы
-	GPIO_Config (GPIOA, Pin_0,  Output, PushPull, HighSpeed, NoPull);			// PA0 - FAN
-	GPIO_Bit_Rst(GPIOA, Pin_0);
-	GPIO_Config (GPIOA, Pin_2,  Output, PushPull, HighSpeed, NoPull);			// PA2 - M_RESET
-	GPIO_Bit_Rst(GPIOA, Pin_2);
-	GPIO_Config (GPIOA, Pin_3,  Output, PushPull, HighSpeed, NoPull);			// PA3 - LOAD
-	GPIO_Bit_Rst(GPIOA, Pin_3);
-	GPIO_Config (GPIOA, Pin_4,  Output, PushPull, HighSpeed, NoPull);			// PA4 - CS
-	GPIO_Bit_Rst(GPIOA, Pin_4);
-	GPIO_Config (GPIOA, Pin_7,  Output, PushPull, HighSpeed, NoPull);			// PA7 - IND
-	GPIO_Bit_Rst(GPIOA, Pin_7);
-	GPIO_Config (GPIOA, Pin_8,  Output, PushPull, HighSpeed, NoPull);			// PA8 - SYNC
-	GPIO_Bit_Rst(GPIOA, Pin_8);
-	GPIO_Config (GPIOA, Pin_15,  Output, PushPull, HighSpeed, NoPull);			// PA15 - SFTY_EN
-	GPIO_Bit_Rst(GPIOA, Pin_15);
-	GPIO_Config (GPIOB, Pin_0,  Output, PushPull, HighSpeed, NoPull);			// PB0 - RLC
-	GPIO_Bit_Rst(GPIOB, Pin_0);
-	GPIO_Config (GPIOB, Pin_1,  Output, PushPull, HighSpeed, NoPull);			// PB1 - RELAY
-	GPIO_Bit_Rst(GPIOB, Pin_1);
-	GPIO_Config (GPIOB, Pin_2,  Output, PushPull, HighSpeed, NoPull);			// PB2 - SNC_TOCU
-	GPIO_Bit_Rst(GPIOB, Pin_2);
-	GPIO_Config (GPIOB, Pin_5,  Output, PushPull, HighSpeed, NoPull);			// PB5 - SREG_CS
-	GPIO_Bit_Rst(GPIOB, Pin_5);
-	GPIO_Config (GPIOB, Pin_6,  Output, PushPull, HighSpeed, NoPull);			// PB6 - CS_GD2
-	GPIO_Bit_Rst(GPIOB, Pin_6);
-	GPIO_Config (GPIOB, Pin_7,  Output, OpenDrain, HighSpeed, NoPull);			// PB7 - TRIG_RST
-	GPIO_Bit_Set(GPIOB, Pin_7);
-	GPIO_Config (GPIOB, Pin_9,  Output, PushPull, HighSpeed, NoPull);			// PB9 - CS_DAC
-	GPIO_Bit_Rst(GPIOB, Pin_9);
-	GPIO_Config (GPIOB, Pin_11,  Output, PushPull, HighSpeed, NoPull);			// PB11 - SREG_OE
-	GPIO_Bit_Rst(GPIOB, Pin_11);
-	GPIO_Config (GPIOB, Pin_12,  Output, PushPull, HighSpeed, NoPull);			// PB12 - CS_GD1
-	GPIO_Bit_Rst(GPIOB, Pin_12);
-	GPIO_Config (GPIOB, Pin_14,  Output, PushPull, HighSpeed, NoPull);			// PB14 - LDAC
-	GPIO_Bit_Rst(GPIOB, Pin_14);
-	GPIO_Config (GPIOC, Pin_13,  Output, PushPull, HighSpeed, NoPull);			// PC13 - LED
-	GPIO_Bit_Rst(GPIOC, Pin_13);
-	GPIO_Config (GPIOC, Pin_14,  Output, PushPull, HighSpeed, NoPull);			// PC14 - Sync_GD
-	GPIO_Bit_Rst(GPIOC, Pin_14);
-	GPIO_Config (GPIOC, Pin_15,  Output, PushPull, HighSpeed, NoPull);			// PC15 - PS_EN
-	GPIO_Bit_Rst(GPIOC, Pin_15);
-
-
+	GPIO_InitPushPullOutput(GPIO_FAN);
+	GPIO_InitPushPullOutput(GPIO_M_RESET);
+	GPIO_InitPushPullOutput(GPIO_LOAD);
+	GPIO_InitPushPullOutput(GPIO_CS);
+	GPIO_InitPushPullOutput(GPIO_IND);
+	GPIO_InitPushPullOutput(GPIO_SYNC);
+	GPIO_InitPushPullOutput(GPIO_SFTY_EN);
+	GPIO_InitPushPullOutput(GPIO_RLC);
+	GPIO_InitPushPullOutput(GPIO_RELAY);
+	GPIO_InitPushPullOutput(GPIO_SNC_TOCU);
+	GPIO_InitPushPullOutput(GPIO_SREG_CS);
+	GPIO_InitPushPullOutput(GPIO_CS_GD2);
+	GPIO_InitPushPullOutput(GPIO_TRIG_RST);
+	GPIO_InitPushPullOutput(GPIO_CS_DAC);
+	GPIO_InitPushPullOutput(GPIO_SREG_OE);
+	GPIO_InitPushPullOutput(GPIO_CS_GD1);
+	GPIO_InitPushPullOutput(GPIO_LDAC);
+	GPIO_InitPushPullOutput(GPIO_LED);
+	GPIO_InitPushPullOutput(GPIO_SYNC_GD);
+	GPIO_InitPushPullOutput(GPIO_PS_EN);
+	//
+	GPIO_SetState(GPIO_TRIG_RST, true);
+	
 	// Входы
-	GPIO_Config (GPIOB, Pin_3, Input, NoPull, HighSpeed, NoPull);				// PB3 - SFTY
-	GPIO_Config (GPIOB, Pin_4, Input, NoPull, HighSpeed, NoPull);				// PB4 - Overflow90
-	GPIO_Config (GPIOB, Pin_8, Input, NoPull, HighSpeed, NoPull);				// PB8 - Overflow10
-	GPIO_Config (GPIOB, Pin_10, Input, NoPull, HighSpeed, NoPull);				// PB10 - Pressure
-
-
+	GPIO_InitInput(GPIO_SFTY, NoPull);
+	GPIO_InitInput(GPIO_OVERFLOW90, NoPull);
+	GPIO_InitInput(GPIO_OVERFLOW10, NoPull);
+	GPIO_InitInput(GPIO_PRESSURE, NoPull);
+	
 	// Альтернативные функции
-	GPIO_Config (GPIOA, Pin_11, AltFn, PushPull, HighSpeed, NoPull);			// PA11 (CAN RX)
-	GPIO_AltFn  (GPIOA, Pin_11, AltFn_9);
-	GPIO_Config (GPIOA, Pin_12, AltFn, PushPull, HighSpeed, NoPull);			// PA12 (CAN TX)
-	GPIO_AltFn  (GPIOA, Pin_12, AltFn_9);
-	GPIO_Config (GPIOA, Pin_9, AltFn, PushPull, HighSpeed, NoPull);				// PA9(USART1 TX)
-	GPIO_AltFn  (GPIOA, Pin_9, AltFn_7);
-	GPIO_Config (GPIOA, Pin_10, AltFn, PushPull, HighSpeed, NoPull);			// PA10(USART1 RX)
-	GPIO_AltFn  (GPIOA, Pin_10, AltFn_7);
-	GPIO_Config (GPIOA, Pin_5, AltFn, PushPull, HighSpeed, NoPull);				// PA5(SPI1_CLK)
-	GPIO_AltFn  (GPIOA, Pin_5, AltFn_5);
-	GPIO_Config (GPIOA, Pin_6, AltFn, PushPull, HighSpeed, NoPull);				// PA6(SPI1_MISO)
-	GPIO_AltFn  (GPIOA, Pin_6, AltFn_5);
-	GPIO_Config (GPIOB, Pin_13, AltFn, PushPull, HighSpeed, NoPull);			// PB13(SPI2_CLK)
-	GPIO_AltFn  (GPIOB, Pin_13, AltFn_5);
-	GPIO_Config (GPIOB, Pin_15, AltFn, PushPull, HighSpeed, NoPull);			// PB15(SPI2_MOSI)
-	GPIO_AltFn  (GPIOB, Pin_15, AltFn_5);
+	GPIO_InitAltFunction(GPIO_ALT_CAN_RX, AltFn_9);
+	GPIO_InitAltFunction(GPIO_ALT_CAN_TX, AltFn_9);
+	GPIO_InitAltFunction(GPIO_ALT_UART_RX, AltFn_7);
+	GPIO_InitAltFunction(GPIO_ALT_UART_TX, AltFn_7);
+	GPIO_InitAltFunction(GPIO_ALT_SPI1_CLK, AltFn_5);
+	GPIO_InitAltFunction(GPIO_ALT_SPI1_MISO, AltFn_5);
+	GPIO_InitAltFunction(GPIO_ALT_SPI2_CLK, AltFn_5);
+	GPIO_InitAltFunction(GPIO_ALT_SPI2_MOSI, AltFn_5);
 }
 //------------------------------------------------------------------------------
 
@@ -111,7 +81,7 @@ void CAN_Config()
 	RCC_CAN_Clk_EN(CAN_1_ClkEN);
 	NCAN_Init(SYSCLK, CAN_BAUDRATE, FALSE);
 	NCAN_FIFOInterrupt(TRUE);
-	NCAN_FilterInit(0, 0, 0);		// Фильтр 0 пропускает все сообщения
+	NCAN_FilterInit(0, 0, 0); // Фильтр 0 пропускает все сообщения
 }
 //------------------------------------------------------------------------------
 
